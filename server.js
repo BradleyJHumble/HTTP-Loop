88a0395e8159b35c014dd61060d5531a5d4707f6
Bradley J. Humble <bradleyjhumble@gmail.com>
2017-10-10 01:19:02 -0500

    looping it 50K times
    .

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var http = require('http');
 var urls = ["http://www.youtube.com/channel/UCDo9msNItILnyF_Y2eHaNQg", "http://www.youtube.com/watch?v=dKnoOdDLy4o", "http://www.youtube.com/watch?v=34UCkAJKt6M", "http://www.youtube.com/watch?v=-Zy28mVeVZw", "http://www.youtube.com/watch?v=IAXcT5ZRzq8"];
 var responses = [];
 var completed_requests = 0;
+for (var riceGum = 1; riceGum < 50000; riceGum++ ){
 for (i in urls) {
     http.get(urls[i], function(res) {
       responses.push(res);
@@ -11,4 +12,6 @@ for (i in urls) {
          console.log(responses);
         }
     });
+}
+console.log("request #:", riceGum);
 }
