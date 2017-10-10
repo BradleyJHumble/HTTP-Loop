ad9db9bfa18b3e4d80464e35e4fa88d92696bbe3
Bradley J. Humble <bradleyjhumble@gmail.com>
2017-10-10 00:54:53 -0500

    set up an non dependency version
    .

---
+++ server.js
@@ -0,0 +1,14 @@
+var http = require('http');    
+var urls = ["https://www.youtube.com/channel/UCDo9msNItILnyF_Y2eHaNQg", "https://www.youtube.com/watch?v=dKnoOdDLy4o", "https://www.youtube.com/watch?v=34UCkAJKt6M", "https://www.youtube.com/watch?v=-Zy28mVeVZw", "https://www.youtube.com/watch?v=IAXcT5ZRzq8"];
+var responses = [];
+var completed_requests = 0;
+for (i in urls) {
+    http.get(urls[i], function(res) {
+      responses.push(res);
+        completed_requests++;
+     if (completed_requests == urls.length) {
+         // looping through array afer request sent, not by responses recieved!
+         console.log(responses);
+        }
+    });
+}
