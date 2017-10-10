8b085bac81c4a09aa054385a4791fb57ba276ae6
Bradley J. Humble <bradleyjhumble@gmail.com>
2017-10-10 01:58:24 -0500

    looping like a mad man.
    .

--- server.js
+++ server.js
@@ -2,16 +2,20 @@ var http = require('http');
 var urls = ["http://www.youtube.com/channel/UCDo9msNItILnyF_Y2eHaNQg", "http://www.youtube.com/watch?v=dKnoOdDLy4o", "http://www.youtube.com/watch?v=34UCkAJKt6M", "http://www.youtube.com/watch?v=-Zy28mVeVZw", "http://www.youtube.com/watch?v=IAXcT5ZRzq8"];
 var responses = [];
 var completed_requests = 0;
-for (var riceGum = 1; riceGum < 50000; riceGum++ ){
-for (i in urls) {
-    http.get(urls[i], function(res) {
-      responses.push(res);
-        completed_requests++;
-     if (completed_requests == urls.length) {
-         // looping through array afer request sent, not by responses recieved!
-         console.log(responses);
+for (var weeb = 1; weeb < 100; weeb++){
+        for (var riceGum = 1; riceGum < 50001; riceGum++ ){
+        for (i in urls) {
+            http.get(urls[i], function(res) {
+              responses.push(res);
+                completed_requests++;
+             if (completed_requests == urls.length) {
+                 // looping through array afer request sent, not by responses recieved!
+                 console.log(responses);
+                }
+            });
         }
-    });
-}
-console.log("request #:", riceGum);
+        console.log("request #:", riceGum);
+        }
+console.log("Loop", weeb);
+
 }
