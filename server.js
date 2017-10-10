215e39de08f7901679db0c45476322dcc5774fe1
Bradley J. Humble <bradleyjhumble@gmail.com>
2017-10-10 02:03:41 -0500

    bug fix: js error of running out of memory...
    .

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ var http = require('http');
 var urls = ["http://www.youtube.com/channel/UCDo9msNItILnyF_Y2eHaNQg", "http://www.youtube.com/watch?v=dKnoOdDLy4o", "http://www.youtube.com/watch?v=34UCkAJKt6M", "http://www.youtube.com/watch?v=-Zy28mVeVZw", "http://www.youtube.com/watch?v=IAXcT5ZRzq8"];
 var responses = [];
 var completed_requests = 0;
-for (var weeb = 1; weeb < 100; weeb++){
-        for (var riceGum = 1; riceGum < 50001; riceGum++ ){
+        for (var riceGum = 1; riceGum < 500001; riceGum++ ){
         for (i in urls) {
             http.get(urls[i], function(res) {
               responses.push(res);
@@ -16,6 +15,4 @@ for (var weeb = 1; weeb < 100; weeb++){
         }
         console.log("request #:", riceGum);
         }
-console.log("Loop", weeb);
 
-}
